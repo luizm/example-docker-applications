eda2531ade6b9ab4b3ec4b27e6097c6ca8c06ac9
Luizm <luizmuller.it@gmail.com>
2019-11-12 12:12:32 -0300

    add docker examples

---
+++ README.md
@@ -0,0 +1,16 @@
+# How to start
+
+- starting
+
+```sh
+    docker-compose build
+    docker-compose up -d
+```
+
+- stopping
+
+```
+    docker-compose down --rmi local
+```
+
+- URL to [access](http://localhost:8080):
---
+++ api/app.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express();
+var uuid = require("node-uuid");
+var logger = require("morgan");
+
+app.use(logger("dev"));
+
+// Routes
+app.get("/api/status", function(req, res) {
+  res.status(200).json({
+    message: "OK"
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
